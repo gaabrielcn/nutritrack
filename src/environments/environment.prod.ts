b97259639928887b7e107f11ca19711d8f2133f4
gaabrielcn <bibicn14@gmail.com>
2025-05-15 08:07:48 -0300

    hospedando no firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCJznZwm3lvlxpEx4OZpM5evbdmm5dcYXc",
+  authDomain: "nutritrack-92c5b.firebaseapp.com",
+  projectId: "nutritrack-92c5b",
+  storageBucket: "nutritrack-92c5b.firebasestorage.app",
+  messagingSenderId: "768561516680",
+  appId: "1:768561516680:web:34100286f33de2a15fcf3d",
+  measurementId: "G-830YJJN1BY"
+  }
 };
